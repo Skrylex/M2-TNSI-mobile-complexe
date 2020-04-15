413acafa4e4bde891463081b16c91c6f78332dff
Maxime Fleury <maxime.fleury0@gmail.com>
2020-04-15 20:37:37 +0200

    Test API

---
+++ lib/pokemonview.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/main.dart';
+import 'package:flutterapp/pokedex.dart';
+import 'package:flutterapp/pokemon.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutterapp/webservice.dart';
+import 'dart:convert' as convert;
+
+
+class PokemonView extends StatefulWidget {
+
+	final String title = "Détails Pokemon";
+	@override
+	_PokemonState createState() => _PokemonState();
+}
+
+class _PokemonState extends State<PokemonView> {
+	String pokemon;
+	int id = 1;
+	@override
+	void initState() {
+		super.initState();
+		pokemon = "Click Me !";
+
+		//WebService.getPokemon(1);
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text(widget.title),
+			),
+			body: GridView.count(
+				// Create a grid with 2 columns. If you change the scrollDirection to
+				// horizontal, this produces 2 rows.
+				crossAxisCount: 2,
+				// Generate 100 widgets that display their index in the List.
+				children: <Widget>[
+					Text(pokemon),
+					RaisedButton(
+						onPressed: () {
+							setState(() {
+							  setPokemon();
+							});
+
+						},
+						child: Text('Clicked '+id.toString()+' times'),
+					)
+				]
+			));
+	}
+
+	void setPokemon(){
+		final myFuture = WebService.getPokemon(id);
+		myFuture.then((resp) {
+			setState(() {
+				pokemon = convert.jsonDecode(resp.body).toString();
+			});
+		});
+		id+=1;
+	}
+}
--- lib/webservice.dart
+++ lib/webservice.dart
@@ -1,24 +1,32 @@
 import 'dart:convert' as convert;
 import 'package:flutterapp/pokedex.dart';
+import 'package:flutterapp/pokemon.dart';
 import 'package:http/http.dart' as http;
 
 class WebService {
-  static String domain = "https://pokeapi.co/api/v2/";
+	static String domain = "https://pokeapi.co/api/v2/";
 
-  static Future<Pokedex> createOrderPokedex() async {
-    var order = await fetchPokedexOrder();
-    return order;
-  }
+	static Future<Pokedex> createOrderPokedex() async {
+		var order = await fetchPokedexOrder();
+		return order;
+	}
 
-  static Future<Pokedex> fetchPokedexOrder() async {
-    var url = WebService.domain + "pokedex/2";
-    var response = await http.get(url);
+	static Future<Pokedex> fetchPokedexOrder() async {
+		var url = WebService.domain + "pokedex/2";
+		var response = await http.get(url);
+		print(convert.jsonDecode(response.body));
+		return Future.delayed(Duration(seconds: 3),
+				() => Pokedex.fromJson(convert.jsonDecode(response.body)));
+	}
 
-    return Future.delayed(Duration(seconds: 3), () => Pokedex.fromJson(convert.jsonDecode(response.body)));
-  }
+	//CreateOrder
+	static Future<http.Response> getPokemon(id) async {
+		var url = WebService.domain + "pokemon/" + id.toString();
+		var response = await http.get(url);
+		return Future.sync(() => response);
+	}
 
-  static void getPokemon(id) async {
-    var url = WebService.domain + "pokemon/" + id;
-    var response = await http.get(url);
-  }
+//Fetch
+// Future<Pokemon> fetchPokemon() => Future.delayed(Duration(seconds: 2), () => 'Salut');
+
 }
